1501d5aeeebf9df85e1055cfde8d26b3f1dd07f4
Deepikasept1995 <deepikachadha52@gmail.com>
2026-02-08 17:07:43 +0530

    practice done 1

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
-function main() {
-  // main change line
-}
+const orders = [
+  { order_id: "O1", status: "DELIVERED", amount: 500 },
+  { order_id: "O2", status: "FAILED", amount: 300 },
+  { order_id: "O3", status: "DELIVERED", amount: 700 },
+  { order_id: "O4", status: "FAILED", amount: 200 }
+];
 
-function local() {
-  // local change
-}
-
-function feature() {
-  // feature change line
-}
-
-function bugFix() {
-  // new bug fix
-}
-
-function goodbye() {
-  // Goodbye
-}
+const result = orders.reduce ((acc, order) => {
+  return {
+    ...acc,
+    [order.status]: {
+      count : (acc[order.status]?.count || 0) + 1,
+      totalAmount : (acc[order.status]?.totalAmount || 0) + order.amount
+    }
+  }
+}, {});
+console.log(result);
