5ee4c382d0c05f6e6ee9e0cadc83655c3ca79b52
Deepikasept1995 <deepikachadha52@gmail.com>
2026-02-10 22:38:16 +0530

    Day 4

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-const users = [
-  { name: "Ravi", balance: 900 },
-  { name: "Neha", balance: 1600 },
-  { name: "Karan", balance: 300 },
-  { name: "Pooja", balance: 2200 }
-];
-
-const eligibleUsers = users.filter(user => user.balance >= 1000);
-eligibleUsers.forEach(user => console.log(`${user.name} is eligible`));
+console.log ("Hello World!");
