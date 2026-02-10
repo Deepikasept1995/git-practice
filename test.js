d7f1eda9a6e057148c9400ddcb422232cd5be7fc
Deepikasept1995 <deepikachadha52@gmail.com>
2026-02-10 22:32:43 +0530

    Day 4

--- test.js
+++ test.js
@@ -1,17 +1,9 @@
-const orders = [
-  { order_id: "O1", status: "DELIVERED", amount: 500 },
-  { order_id: "O2", status: "FAILED", amount: 300 },
-  { order_id: "O3", status: "DELIVERED", amount: 700 },
-  { order_id: "O4", status: "FAILED", amount: 200 }
+const users = [
+  { name: "Ravi", balance: 900 },
+  { name: "Neha", balance: 1600 },
+  { name: "Karan", balance: 300 },
+  { name: "Pooja", balance: 2200 }
 ];
 
-const result = orders.reduce ((acc, order) => {
-  return {
-    ...acc,
-    [order.status]: {
-      count : (acc[order.status]?.count || 0) + 1,
-      totalAmount : (acc[order.status]?.totalAmount || 0) + order.amount
-    }
-  }
-},{});
-console.log(result);
+const eligibleUsers = users.filter(user => user.balance >= 1000);
+eligibleUsers.forEach(user => console.log(`${user.name} is eligible`));
