53f8b4caf3f9b8c403ea323337681ce5882da3a6
Deepikasept1995 <deepikachadha52@gmail.com>
2026-02-19 21:17:49 +0530

    Day 7

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-console.log([] + []);
-console.log([] + {});
-console.log({} + []);
+const users = [
+  { name: "Ravi", balance: 900 },
+  { name: "Neha", balance: 1600 },
+  { name: "Karan", balance: 300 }
+];
+const updatedUsers = users.map(user => ({
+  ...user,
+  eligible: user.balance >= 1000
+}));
+
+console.log(updatedUsers);
