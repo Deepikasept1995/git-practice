36dad52986c7424c32a4500a2d2968603e7cdf50
Deepikasept1995 <deepikachadha52@gmail.com>
2026-02-20 21:25:48 +0530

    Day 8

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-const users = [
-  { name: "Ravi", balance: 900 },
-  { name: "Neha", balance: 1600 },
-  { name: "Karan", balance: 300 }
+const orders = [
+  {id:1, amount:500, status:"DELIVERED"},
+  {id:2, amount:2000, status:"DELIVERED"},
+  {id:3, amount:1500, status:"PENDING"},
+  {id:4, amount:3000, status:"DELIVERED"},
 ];
-const updatedUsers = users.map(user => ({
-  ...user,
-  eligible: user.balance >= 1000
-}));
 
-console.log(updatedUsers);
+const result = orders.reduce(
+  (acc, order) => {
+    if (order.status === "DELIVERED") {
+      acc.totalamount += order.amount;
+      acc.count += 1;
+    }
+    return acc;
+  },
+  { totalamount: 0, count: 0 }
+);
+
+const average = result.count > 0 ? (result.totalamount / result.count).toFixed(2) : 0.00;
+
+console.log({status: result.count > 0 ? "DELIVERED" : "PENDING", average: average});
