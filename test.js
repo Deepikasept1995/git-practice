4dcf8aab881eb571c5810b7c40f27354ac8ebd87
Deepikasept1995 <deepikachadha52@gmail.com>
2026-02-28 16:37:51 +0530

    Day 13

--- test.js
+++ test.js
@@ -1,32 +1,14 @@
-const orders = [
-  { id: 1, user: "Ravi", amount: 1200, status: "DELIVERED", items: 3, createdAt: "2026-02-25T10:10:00+05:30" },
-  { id: 2, user: "Neha", amount: 800, status: "PENDING", items: 1, createdAt: "2026-02-25T11:20:00+05:30" },
-  { id: 3, user: "Ravi", amount: 2000, status: "DELIVERED", items: 4, createdAt: "2026-02-26T09:15:00+05:30" },
-  { id: 4, user: "Karan", amount: 500, status: "CANCELLED", items: 2, createdAt: "2026-02-26T21:40:00+05:30" },
-  { id: 5, user: "Neha", amount: 1500, status: "DELIVERED", items: 2, createdAt: "2026-02-27T08:05:00+05:30" },
-  { id: 6, user: "Pooja", amount: 2600, status: "DELIVERED", items: 5, createdAt: "2026-02-27T19:30:00+05:30" }
-];
+let count = 5;
 
-function topUserToday(orders) {
-    const today = "2026-02-27"; // safer for testing
+const timer = setInterval(function() {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    console.log(`Random number: ${randomNumber}`);
+    count--;
 
-    return orders.reduce((acc, order) => {
+    if (count === 0) {
+        clearInterval(timer);
+        console.log('Stopped after 5 prints');
+    }
+}, 1000);
 
-        if (order.createdAt.startsWith(today) && order.status === "DELIVERED") {
-
-            // 1️⃣ Track revenue per user
-            acc[order.user] = (acc[order.user] || 0) + order.amount;
-
-            // 2️⃣ Check if this user is now highest
-            if (acc[order.user] > acc.revenue) {
-                acc.user = order.user;
-                acc.revenue = acc[order.user];
-            }
-        }
-
-        return acc;
-
-    }, { user: "", revenue: 0 });
-}
-
-console.log(topUserToday(orders));
+console.log('Timer started!'); 
