e1180bcd10078a2f0e11b67a567d8b8a7326005b
Deepikasept1995 <deepikachadha52@gmail.com>
2026-03-09 22:28:06 +0530

    Day 17

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
-const car = {
-  brand: "BMW",
-  speed: "50MPH",
-  drive: function() {
-    console.log (`${this.brand} is driving at ${this.speed}`);
+const orders = [
+ {product:"Phone", amount:500},
+ {product:"Laptop", amount:1000},
+ {product:"Phone", amount:700},
+ {product:"Tablet", amount:300},
+ {product:"Laptop", amount:800}
+];
+
+const totals = orders.reduce((acc, order) => {
+
+  acc[order.product] = (acc[order.product] || 0) + order.amount;
+
+  return acc;
+
+}, {});
+
+const result = Object.entries(totals).reduce ((acc, [product, totals]) => {
+  if (totals > acc.maxRevenue) {
+    acc.maxRevenue = totals;
+    acc.topProduct = product;
   }
-};
-car.drive();
+
+  return acc;
+
+}, {topProduct: null, maxRevenue: 0});
+
+console.log(result);
