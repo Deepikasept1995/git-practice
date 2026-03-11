990646a934a73d85125ab80fed543635f0bf9706
Deepikasept1995 <deepikachadha52@gmail.com>
2026-03-11 22:31:39 +0530

    Day 19

--- test.js
+++ test.js
@@ -1,21 +1,9 @@
-const nums = [1,2,2,3,3,3,4];
+const key = "age";
 
-const result = nums.reduce((acc, num) => {
+const person = {
+  age: 30,
+  key: 50
+};
 
-  acc.counts[num] = (acc.counts[num] || 0) + 1;
-
-  if (acc.counts[num] > acc.count) {
-    acc.count = acc.counts[num];
-    acc.mostFrequent = num;
-  }
-
-  return acc;
-
-}, {counts:{}, mostFrequent:null, count:0});
-
-console.log({
-  mostFrequent: result.mostFrequent,
-  count: result.count
-});
-
-
+console.log(person.key);
+console.log(person[key]);
