6d1842f44d89f64a62b21597b1596c60f8a1cac8
Deepikasept1995 <deepikachadha52@gmail.com>
2026-03-14 13:16:49 +0530

    Day 21

--- test.js
+++ test.js
@@ -1,121 +1,64 @@
-const orders = [
- {user:"Ravi", amount:200},
- {user:"Neha", amount:300},
- {user:"Ravi", amount:150},
- {user:"Amit", amount:500},
- {user:"Neha", amount:100}
-];
-
-const result1 = orders.reduce ((acc, order) => {
-  if (!acc[order.user]) {
-    acc[order.user] = [];
-  }
-  acc[order.user].push(order.amount);
-  return acc;
-},{});
-console.log(result1);
-
-
-const nums = [10,5,8,20,15];
-
-const result2 = nums.reduce(
-  (acc, num) => {
-    if (num > acc.largest) {
-      acc.secondLargest = acc.largest;
-      acc.largest = num;
-    } else if (num > acc.secondLargest && num < acc.largest) {
-      acc.secondLargest = num;
-    }
-    return acc;
-  },
-  { largest: -Infinity, secondLargest: -Infinity }
-).secondLargest;
-
-console.log(result2);
-
-
-const arr = [1,[2,3],4,[5,6]];
-
-function flatten(arr) {
-  return arr.reduce ((acc, val) => {
-    if (Array.isArray(val)) {
-      acc.push(...flatten(val));
-    } else {
-      acc.push(val);
-    }
-    return acc;
-  },[]);
+function fetchOrders() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, user: "Ravi", amount: 200, status: "DELIVERED" },
+        { id: 2, user: "Neha", amount: 500, status: "PENDING" },
+        { id: 3, user: "Ravi", amount: 400, status: "DELIVERED" },
+        { id: 4, user: "Amit", amount: 300, status: "CANCELLED" },
+        { id: 5, user: "Neha", amount: 250, status: "DELIVERED" },
+        { id: 6, user: "Amit", amount: 700, status: "DELIVERED" }
+      ]);
+    }, 1000);
+  });
 }
-console.log(flatten([1,[2,3],[4,[5,6]]]));
 
+function fetchUsers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { name: "Ravi", age: 23, city: "Delhi" },
+        { name: "Neha", age: 17, city: "Mumbai" },
+        { name: "Amit", age: 30, city: "Bangalore" }
+      ]);
+    }, 1000);
+  });
+}
 
-const orders1 = [
- {user:"Ravi", amount:200},
- {user:"Neha", amount:500},
- {user:"Ravi", amount:400},
- {user:"Amit", amount:300}
-];
+async function processData() {
+  try {
+    const orders = await fetchOrders();
+    const users = await fetchUsers();
+    const totalOrders = orders.length;
+    const totalRevenue =  orders.reduce ((sum, order) => sum + order.amount);
+    const statusCount = orders.reduce ((acc, order) => {acc[order.status] = (acc[order.status] || 0) + 1;
+      return acc;
+    },{});  
+    const revenuePerUser = orders.reduce((acc, order) => {acc[order.user] = (acc[order.user] || 0) + order.amount;
+    return acc;
+    },{});
+    const topSpender = Object.keys(revenuePerUser).reduce((top, user) => {
+      return revenuePerUser[user] > revenuePerUser[top] ? user : top;
+    });
+    const adultUsers = users.filter ((user) => user.age >= 18)
+    .map((user) => `${user.name}-${user.city}`);
+    
+    console.log(`Total orders: ${totalOrders}`);
+    console.log(`Total orders: ${totalRevenue}`);
+    console.log("Status Counts:", statusCount);
+    console.log("User Totals:", revenuePerUser);
+    console.log("Top Spender:",topSpender);
+    console.log("Adult Users:",adultUsers);
 
-const totals = orders1.reduce((acc, order) => {
-  acc[order.user] = (acc[order.user] || 0) + order.amount;
-  return acc;
-}, {});
-
-const result4 = Object.keys(totals).reduce((maxUser, user) => {
-  return totals[user] > totals[maxUser] ? user : maxUser;
-});
-
-console.log(result4);
-
-const str = "javascript";
-
-const result5 = str.split("").reduce ((acc, st) => {
-  acc[st] = (acc[st] || 0) + 1;
-  return acc;
-},{});
-
-console.log(result5);
-
-const orders3 = [
-  { id: 1, user: "Ravi", amount: 200, status: "DELIVERED" },
-  { id: 2, user: "Neha", amount: 500, status: "PENDING" },
-  { id: 3, user: "Ravi", amount: 400, status: "DELIVERED" },
-  { id: 4, user: "Amit", amount: 300, status: "CANCELLED" },
-  { id: 5, user: "Neha", amount: 250, status: "DELIVERED" },
-  { id: 6, user: "Amit", amount: 700, status: "DELIVERED" }
-];
-
-const userMetrics = orders3.reduce((acc, order) => {
-
-  acc.totalOrders += 1;
-
-  acc.statusCounts[order.status] =
-    (acc.statusCounts[order.status] || 0) + 1;
-
-  if (order.status === "DELIVERED") {
-    acc.deliveredRevenue += order.amount;
+  } catch (error) {
+    console.log("Error", error);
   }
+}
+processData();
 
-  acc.userTotals[order.user] =
-    (acc.userTotals[order.user] || 0) + order.amount;
-
-  return acc;
-
-}, {
-  totalOrders: 0,
-  deliveredRevenue: 0,
-  statusCounts: {},
-  userTotals: {}
-});
-
-const topSpender = Object.keys(userMetrics.userTotals).reduce(
-  (maxUser, user) => {
-    return userMetrics.userTotals[user] >
-      userMetrics.userTotals[maxUser]
-      ? user
-      : maxUser;
-  }
-);
-
-console.log(userMetrics);
-console.log(topSpender);
+//Total orders: 6
+//Delivered revenue: 1550
+//Status counts: { DELIVERED: 4, PENDING: 1, CANCELLED: 1 }
+//User totals: { Ravi: 600, Neha: 750, Amit: 1000 }
+//Top spender: Amit
+//Adult users: [ 'Ravi-Delhi', 'Amit-Bangalore' ]
